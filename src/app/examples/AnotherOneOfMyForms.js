121c3f366024e510f4907a6fa0406b55bc353d3b
Francesco Giordano <francesco.giordano@buildo.io>
2016-12-13 17:06:24 +0100

    another example

--- src/app/examples/AnotherOneOfMyForms.js
+++ src/app/examples/AnotherOneOfMyForms.js
@@ -3,15 +3,9 @@ import t from 'tcomb';
 import { props } from 'tcomb-react';
 import { skinnable, pure } from 'revenge';
 import View from 'react-flexview';
-import { Dropdown, Toggle } from 'buildo-react-components';
 import formo from 'formo';
-import compact from 'lodash/compact';
 import range from 'lodash/range';
-import map from 'lodash/map';
 import printJSON from 'printJSON';
-
-import 'buildo-react-components/src/dropdown/dropdown.scss';
-import 'buildo-react-components/src/toggle/toggle.scss';
 
 const style = ({ isValid, active, touched }) => ({
   borderColor: touched ? isValid ? 'green' : 'red' : 'black',
@@ -19,45 +13,20 @@ const style = ({ isValid, active, touched }) => ({
 });
 
 const formoConfig = () => ({
-  number: {
-    validations: value => {
-      return {
-        valid: /^\d{16}$/.test(value) ? null : 'Insert a valid credit card number'
-      };
-    }
-  },
-  cardholder: {
+  rating: {
+    initialValue: 0,
     validations: value => ({
-      valid: /^[A-Z][a-z]+\s[A-Z][a-z]+$/.test(value) ? null : 'Insert a valid name',
-      required: value ? null : 'The cardholder name is required'
+      required: value > 0
     })
   },
-  expirationMonth: {
-    validations: value => {
-      return {
-        valid: /^\d{2}$/.test(value) ? null : 'Expiration month is not valid',
-        required: value ? null : 'The expiration month is required'
-      };
-    }
+  feedback: {
+    initialValue: ''
   },
-  expirationYear: {
-    validations: value => {
-      return {
-        valid: /^\d{4}$/.test(value) ? null : 'Expiration year is not valid',
-        required: value ? null : 'The expiration year is required'
-      };
-    }
-  },
-  cvv: {
-    validations: value => {
-      return {
-        valid: /^\d{3}$/.test(value) ? null : 'CVV is not valid',
-        required: value ? null : 'The CVV is required'
-      };
-    }
-  },
-  saveData: {
-    initialValue: false
+  securityQuestion: {
+    initialValue: '',
+    validations: value => ({
+      incorrect: parseInt(value) === (8 + 5) ? null : 'the answer is incorrect'
+    })
   }
 });
 
@@ -68,99 +37,48 @@ const inputStyle = {
   width: '100%'
 };
 
+const startRatingStyle = {
+  fontSize: 32,
+  margin: '0 5px',
+  cursor: 'pointer'
+};
+
 const Input = ({ onChange, style = {}, ...others }) => <input onChange={e => onChange(e.target.value)} {...others} style={{ ...inputStyle, ...style }} />;
+
+const StarRating = ({ rating, onChange }) => (
+  <View>
+    {range(5).map((star, i) => <View style={{ ...startRatingStyle, opacity: rating >= (i + 1) ? 1 : .5 }} onClick={() => { onChange(i + 1); }}>⭐</View>)}
+  </View>
+);
 
 @formo(formoConfig)
 @pure
 @skinnable()
 @props({
-  number: t.Object, // specify
-  cardholder: t.Object, //specify
-  expirationMonth: t.Object, //specify
-  expirationYear: t.Object,
-  cvv: t.Object,
-  saveData: t.Object
+  rating: t.Object, // specify
+  feedback: t.Object, //specify
+  securityQuestion: t.Object
 })
 export default class AnotherOneOfMyForms extends React.Component {
 
-  template({ number, cardholder, expirationMonth, expirationYear, cvv, saveData }) {
+  template({ rating, feedback, securityQuestion }) {
 
     return (
       <View basis='50%'>
         <View column width={600}>
 
           <View>
-            <Input
-              value={cardholder.value}
-              onFocus={cardholder.setActive}
-              onBlur={cardholder.unsetActive}
-              onChange={cardholder.update}
-              style={style(cardholder)}
-              placeholder='cardholder'
-            />
-            {cardholder.touched && compact(map(cardholder.validations)).join(', ')}
+            <StarRating rating={rating.value} onChange={rating.update} />
           </View>
 
           <View>
-            <Input
-              value={number.value}
-              onFocus={number.setActive}
-              onBlur={number.unsetActive}
-              onChange={number.update}
-              style={style(number)}
-              placeholder='card number'
-            />
-            {number.touched && compact(map(number.validations)).join(', ')}
-          </View>
-
-          <View style={style(expirationMonth)}>
-            <Dropdown
-              clearable
-              value={expirationMonth.value}
-              options={range(12).map(x => `00${x + 1}`.slice(-2)).map(x => ({ value: x, label: x }))}
-              onFocus={expirationMonth.setActive}
-              onBlur={expirationMonth.unsetActive}
-              onChange={expirationMonth.update}
-              placeholder='expiration month'
-            />
-            {expirationMonth.touched && compact(map(expirationMonth.validations)).join(', ')}
-          </View>
-
-          <View style={style(expirationYear)}>
-            <Dropdown
-              clearable
-              value={expirationYear.value}
-              options={range(2016, 2026).map(x => `${x}`).map(x => ({ value: x, label: x }))}
-              onFocus={expirationYear.setActive}
-              onBlur={expirationYear.unsetActive}
-              onChange={expirationYear.update}
-              placeholder='expiration year'
-            />
-            {expirationYear.touched && compact(map(expirationYear.validations)).join(', ')}
+            <textarea style={style(feedback)} value={feedback.value} onChange={e => feedback.update(e.target.value)} />
           </View>
 
           <View>
-            <Input
-              value={cvv.value}
-              onFocus={cvv.setActive}
-              onBlur={cvv.unsetActive}
-              onChange={cvv.update}
-              style={style(cvv)}
-              placeholder='cvv'
-            />
-            {cvv.touched && compact(map(cvv.validations)).join(', ')}
+            <label>8 + 5 =</label>
+            <Input style={style(securityQuestion)} type='number' value={securityQuestion.value} onChange={securityQuestion.update} />
           </View>
-
-          <View>
-            Save Data?
-            <Toggle
-              value={saveData.value}
-              onChange={saveData.update}
-              style={style(saveData)}
-            />
-            {saveData.touched && compact(map(saveData.validations)).join(', ')}
-          </View>
-
 
         </View>
 
@@ -169,7 +87,7 @@ export default class AnotherOneOfMyForms extends React.Component {
           <textarea
             readOnly
             style={{ height: 500, width: 500, fontFamily: 'monospace' }}
-            value={printJSON({ number, cardholder, expirationMonth, expirationYear, cvv, saveData })}
+            value={printJSON({ rating, feedback, securityQuestion })}
           />
         </View>
       </View>
