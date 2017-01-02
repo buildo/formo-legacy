4aa205cd33ec7f68c860a560998fc8a94f1bb1a8
Giovanni Gonzaga <giovanni@buildo.io>
2017-01-02 22:13:26 +0100

    change helpers signature and update form prop to `fields`

--- test/formo.test.js
+++ test/formo.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import formo from '../src/app/formo';
 import renderer from 'react-test-renderer';
-import { shallow as _shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
-const getProps = (form = {}, validations = undefined) => {
+const getProps = ({ fields = {}, validations, onChange = () => {} } = {}) => {
   const C = formo(props => <div {...props} />);
-  return renderer.create(<C form={form} validations={validations} onChange={() => {}} />).toJSON().props;
+  return renderer.create(<C fields={fields} validations={validations} onChange={onChange} />).toJSON().props;
 };
 
-const shallow = (form = {}, validations = undefined) => {
+const shallowRender = ({ fields = {}, validations, onChange = () => {} } = {}) => {
   const C = formo(props => <div {...props} />);
-  return _shallow(<C form={form} validations={validations} onChange={() => {}} />);
+  return shallow(<C fields={fields} validations={validations} onChange={onChange} />);
 };
 
 it('can be rendered', () => {
@@ -23,7 +23,7 @@ it('always passes the form meta prop', () => {
 });
 
 it('passes a prop for every given field', () => {
-  const props = getProps({ email: {}, password: {} });
+  const props = getProps({ fields: { email: {}, password: {} } });
   expect(props.email).toBeDefined();
   expect(props.password).toBeDefined();
 });
@@ -31,53 +31,53 @@ it('passes a prop for every given field', () => {
 describe('[field].value', () => {
 
   it('defaults to initialValue if no value is provided', () => {
-    const { email } = getProps({ email: { initialValue: 'test' } });
+    const { email } = getProps({ fields: { email: { initialValue: 'test' } } });
     expect(email.value).toBe('test');
   });
 
   it('defaults to initialValue if value is undefined', () => {
-    const { email } = getProps({ email: { initialValue: 'test', value: undefined } });
+    const { email } = getProps({ fields: { email: { initialValue: 'test', value: undefined } } });
     expect(email.value).toBe('test');
   });
 
   xit('defaults to initialValue if value is null', () => {
-    const { email } = getProps({ email: { initialValue: 'test', value: null } });
+    const { email } = getProps({ fields: { email: { initialValue: 'test', value: null } } });
     expect(email.value).toBe('test');
   });
 
   xit('defaults to initialValue if value is \'\'', () => {
-    const { email } = getProps({ email: { initialValue: 'test', value: '' } });
+    const { email } = getProps({ fields: { email: { initialValue: 'test', value: '' } } });
     expect(email.value).toBe('test');
   });
 
   it('defaults to undefined if no value or initialValue are provided', () => {
-    const { email } = getProps({ email: { } });
+    const { email } = getProps({ fields: { email: { } } });
     expect(email.value).toBe(undefined);
   });
 
   it('can be updated', () => {
-    const rendered = shallow({ email: { value: 'foo' } });
+    const rendered = shallowRender({ fields: { email: { value: 'foo' } } });
     expect(rendered.props().email.value).toBe('foo');
     rendered.props().email.update('bar');
     expect(rendered.props().email.value).toBe('bar');
   });
 
   it('can be set for the first time', () => {
-    const rendered = shallow({ email: { } });
+    const rendered = shallowRender({ fields: { email: { } } });
     rendered.props().email.update('bar');
     expect(rendered.props().email.value).toBe('bar');
   });
 
   it('can be updated updating props', () => {
-    const rendered = shallow({ email: { value: 'foo' } });
+    const rendered = shallowRender({ fields: { email: { value: 'foo' } } });
     expect(rendered.props().email.value).toBe('foo');
-    rendered.setProps({ form: { email: { value: 'bar' } } });
+    rendered.setProps({ fields: { email: { value: 'bar' } } });
     expect(rendered.props().email.value).toBe('bar');
   });
 
   it('can be set for the first time updating props', () => {
-    const rendered = shallow({ email: { } });
-    rendered.setProps({ form: { email: { value: 'bar' } } });
+    const rendered = shallowRender({ fields: { email: { } } });
+    rendered.setProps({ fields: { email: { value: 'bar' } } });
     expect(rendered.props().email.value).toBe('bar');
   });
 
@@ -86,23 +86,23 @@ describe('[field].value', () => {
 describe('[field].isValid', () => {
 
   it('is computed and true even if no validations are given', () => {
-    const props = getProps({ email: { value: 'foo' } });
+    const props = getProps({ fields: { email: { value: 'foo' } } });
     expect(props.email.isValid).toBe(true);
   });
 
   it('is computed at first render', () => {
-    const props = getProps(
-      { email: { value: 'foo' } },
-      { email: () => 'error' }
-    );
+    const props = getProps({
+      fields: { email: { value: 'foo' } },
+      validations: { email: () => 'error' }
+    });
     expect(props.email.isValid).toBe(false);
   });
 
   it('is recomputed changing value', () => {
-    const rendered = shallow(
-      { email: { value: 'error' } },
-      { email: v => v === 'error' ? 'error' : null }
-    );
+    const rendered = shallowRender({
+      fields: { email: { value: 'error' } },
+      validations: { email: v => v === 'error' ? 'error' : null }
+    });
     expect(rendered.props().email.isValid).toBe(false);
     rendered.props().email.update('foo');
     expect(rendered.props().email.isValid).toBe(true);
@@ -111,32 +111,32 @@ describe('[field].isValid', () => {
   });
 
   it('is recomputed changing value via props', () => {
-    const rendered = shallow(
-      { email: { value: 'error' } },
-      { email: v => v === 'error' ? 'error' : null }
-    );
+    const rendered = shallowRender({
+      fields: { email: { value: 'error' } },
+      validations: { email: v => v === 'error' ? 'error' : null }
+    });
     expect(rendered.props().email.isValid).toBe(false);
-    rendered.setProps({ form: { email: { value: 'foo' } } });
+    rendered.setProps({ fields: { email: { value: 'foo' } } });
     expect(rendered.props().email.isValid).toBe(true);
-    rendered.setProps({ form: { email: { value: 'error' } } });
+    rendered.setProps({ fields: { email: { value: 'error' } } });
     expect(rendered.props().email.isValid).toBe(false);
   });
 
   it('is ignored if passed via props', () => {
-    const rendered = shallow(
-      { email: { value: 'error', isValid: true } },
-      { email: () => 'error' }
-    );
+    const rendered = shallowRender({
+      fields: { email: { value: 'error', isValid: true } },
+      validations: { email: () => 'error' }
+    });
     expect(rendered.props().email.isValid).toBe(false);
   });
 
   it('is ignored if updated via props', () => {
-    const rendered = shallow(
-      { email: { value: 'error' } },
-      { email: () => 'error' }
-    );
+    const rendered = shallowRender({
+      fields: { email: { value: 'error' } },
+      validations: { email: () => 'error' }
+    });
     expect(rendered.props().email.isValid).toBe(false);
-    rendered.setProps({ form: { email: { value: 'error', isValid: true } } });
+    rendered.setProps({ fields: { email: { value: 'error', isValid: true } } });
     expect(rendered.props().email.isValid).toBe(false);
   });
 
@@ -145,19 +145,19 @@ describe('[field].isValid', () => {
 describe('[field].touched', () => {
 
   it('defaults to false for every field', () => {
-    const rendered = shallow({ email: { touched: true }, password: { } });
+    const rendered = shallowRender({ fields: { email: { touched: true }, password: { } } });
     expect(rendered.props().email.touched).toBe(true);
     expect(rendered.props().password.touched).toBe(false);
   });
 
   it('is true after an unsetActive()', () => {
-    const rendered = shallow({ email: { } });
+    const rendered = shallowRender({ fields: { email: { } } });
     rendered.props().email.unsetActive();
     expect(rendered.props().email.touched).toBe(true);
   });
 
   it('can be updated via props', () => {
-    const rendered = shallow({ email: { } });
+    const rendered = shallowRender({ fields: { email: { } } });
     rendered.setProps({ email: { touched: true } });
     expect(rendered.props().email.touched).toBe(true);
   });
@@ -167,19 +167,19 @@ describe('[field].touched', () => {
 describe('[field].active', () => {
 
   it('defaults to false for every field', () => {
-    const rendered = shallow({ email: { }, password: { active: true } });
+    const rendered = shallowRender({ fields: { email: { }, password: { active: true } } });
     expect(rendered.props().email.active).toBe(false);
     expect(rendered.props().password.active).toBe(true);
   });
 
   it('is true after a setActive()', () => {
-    const rendered = shallow({ email: { } });
+    const rendered = shallowRender({ fields: { email: { } } });
     rendered.props().email.setActive();
     expect(rendered.props().email.active).toBe(true);
   });
 
   it('can be updated via props', () => {
-    const rendered = shallow({ email: { } });
+    const rendered = shallowRender({ fields: { email: { } } });
     rendered.setProps({ email: { active: true } });
     expect(rendered.props().email.active).toBe(true);
   });
@@ -187,21 +187,21 @@ describe('[field].active', () => {
   describe('is true for at most a single field simultaneously', () => {
 
     it('defaults to the first active field to be considered active', () => {
-      const rendered = shallow({ email: { active: true }, password: { active: true } });
+      const rendered = shallowRender({ fields: { email: { active: true }, password: { active: true } } });
       expect(rendered.props().email.active).toBe(true);
       expect(rendered.props().password.active).toBe(false);
     });
 
     it('is updated accordingly on all fields when activating a field', () => {
-      const rendered = shallow({ email: { active: true }, password: { } });
+      const rendered = shallowRender({ fields: { email: { active: true }, password: { } } });
       rendered.props().password.setActive();
       expect(rendered.props().email.active).toBe(false);
       expect(rendered.props().password.active).toBe(true);
     });
 
     it('defaults to the first active field to be considered active, when updated via props', () => {
-      const rendered = shallow({ email: { }, password: { active: true } });
-      rendered.setProps({ form: { email: { active: true }, password: { active: true } } });
+      const rendered = shallowRender({ fields: { email: { }, password: { active: true } } });
+      rendered.setProps({ fields: { email: { active: true }, password: { active: true } } });
       expect(rendered.props().email.active).toBe(true);
       expect(rendered.props().password.active).toBe(false);
     });
@@ -213,7 +213,7 @@ describe('[field].active', () => {
 describe('form.touchAll()', () => {
 
   it('sets every field as touched', () => {
-    const rendered = shallow({ email: { }, password: { } });
+    const rendered = shallowRender({ fields: { email: { }, password: { } } });
     rendered.props().form.touchAll();
     expect(rendered.props().email.touched).toBe(true);
     expect(rendered.props().password.touched).toBe(true);
@@ -224,7 +224,7 @@ describe('form.touchAll()', () => {
 describe('form.clearValues()', () => {
 
   it('should reset a value to initialValue', () => {
-    const rendered = shallow({ email: { initialValue: 'initial' } });
+    const rendered = shallowRender({ fields: { email: { initialValue: 'initial' } } });
     rendered.props().email.update('new');
     expect(rendered.props().email.value).toBe('new');
     rendered.props().form.clearValues();
@@ -232,7 +232,7 @@ describe('form.clearValues()', () => {
   });
 
   it('should reset a value to undefined if no initialValue is provided', () => {
-    const rendered = shallow({ email: { } });
+    const rendered = shallowRender({ fields: { email: { } } });
     rendered.props().email.update('new');
     expect(rendered.props().email.value).toBe('new');
     rendered.props().form.clearValues();
@@ -240,7 +240,7 @@ describe('form.clearValues()', () => {
   });
 
   it('should reset a value to initialValue even if a value is passed initially', () => {
-    const rendered = shallow({ email: { initialValue: 'initial', value: 'initialValue' } });
+    const rendered = shallowRender({ fields: { email: { initialValue: 'initial', value: 'initialValue' } } });
     expect(rendered.props().email.value).toBe('initialValue');
     rendered.props().form.clearValues();
     expect(rendered.props().email.value).toBe('initial');
@@ -251,21 +251,21 @@ describe('form.clearValues()', () => {
 describe('form.isChanged', () => {
 
   it('should be false if value and initial value are the same', () => {
-    expect(getProps({ email: { value: '', initialValue: '' } }).form.isChanged).toBe(false);
+    expect(getProps({ fields: { email: { value: '', initialValue: '' } } }).form.isChanged).toBe(false);
   });
 
   it('should be false if value and initial value are the same, non falsy', () => {
-    expect(getProps({ email: { value: 'a', initialValue: 'a' } }).form.isChanged).toBe(false);
+    expect(getProps({ fields: { email: { value: 'a', initialValue: 'a' } } }).form.isChanged).toBe(false);
   });
 
   it('should be false if value and initial value are "adequately equal": \'\' and null', () => {
-    expect(getProps({ email: { value: '', initialValue: null } }).form.isChanged).toBe(false);
-    expect(getProps({ email: { initialValue: '', value: null } }).form.isChanged).toBe(false);
+    expect(getProps({ fields: { email: { value: '', initialValue: null } } }).form.isChanged).toBe(false);
+    expect(getProps({ fields: { email: { initialValue: '', value: null } } }).form.isChanged).toBe(false);
   });
 
   it('should be false if value and initial value are "adequately equal": \'\' and undefined', () => {
-    expect(getProps({ email: { value: '', initialValue: undefined } }).form.isChanged).toBe(false);
-    expect(getProps({ email: { initialValue: '', value: undefined } }).form.isChanged).toBe(false);
+    expect(getProps({ fields: { email: { value: '', initialValue: undefined } } }).form.isChanged).toBe(false);
+    expect(getProps({ fields: { email: { initialValue: '', value: undefined } } }).form.isChanged).toBe(false);
   });
 
 });
