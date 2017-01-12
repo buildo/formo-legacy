1889842ae124cc86b56d485516259de0cf9d6495
Francesco Giordano <francesco.giordano@buildo.io>
2017-01-12 18:45:50 +0100

    set structs strict by default

--- src/client.js
+++ src/client.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import t from 'tcomb';
+
+// set structs strict by default
+t.struct.strict = true;
 
 ReactDOM.render(<App />, document.getElementById('app'));
