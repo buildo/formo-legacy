499579860b8cc9e57c3398020e6685a7fbefd393
Francesco Giordano <francesco.giordano@buildo.io>
2017-01-12 18:26:33 +0100

    create a method to evaluate validations and clean output

--- src/app/formo/formo.js
+++ src/app/formo/formo.js
@@ -63,6 +63,13 @@ const set = (key) => (value) => (object) => ({
 
 const firstDefined = (...args) => find(args, x => x !== void 0 && x !== null);
 
+const isNotEmptyString = (value) => !t.String.is(value) || value === '';
+const Validations = dict(t.String, t.String, 'Validations');
+const evalValidations = (validationsFn, value, otherValues) => {
+  const evaluated = validationsFn(value, otherValues);
+  return Validations(omitBy(evaluated, isNotEmptyString));
+};
+
 const innerSet = (object) => (firstKey) => (secondKey) => (value) => {
   const newFirstKeyObject = set(secondKey)(value)(object[firstKey]);
   return set(firstKey)(newFirstKeyObject)(object);
@@ -128,8 +135,8 @@ const formo = (Component) => {
 
     fieldsWithValidations = fields => {
       return mapValues(fields, (field) => {
-        const validations = omitBy(field.validations(field.value, mapValues(fields, 'value')), x => x === null);
-        const isValid = every(validations, isNull);
+        const validations = evalValidations(field.validations, field.value, mapValues(fields, 'value'));
+        const isValid = isEqual(validations, {});
         return {
           ...field,
           validations,
@@ -231,7 +238,7 @@ const formo = (Component) => {
     formIsChanged = fields => some(fields, this.isChanged);
 
     formIsValid = (fields, formValidations) => {
-      return every(fields, 'isValid') && every(formValidations, x => x === null);
+      return every(fields, 'isValid') && isEqual(formValidations, {});
     }
 
     enforceOnlyOneActive = (fields) => {
@@ -251,11 +258,11 @@ const formo = (Component) => {
 
     makeForm = ({ fields: rawFields, validations }) => {
       const fields = flowRight(this.fieldsWithValidations, this.enforceOnlyOneActive, this.fieldsAreChanged, this.getFields)(rawFields);
-      const formValidations = (validations.form || returnEmpty)(mapValues(fields, 'value'));
+      const formValidations = evalValidations(validations.form || returnEmpty, mapValues(fields, 'value'));
       return {
         touched: some(fields, 'touched'),
         allTouched: every(fields, 'touched'),
-        validations: omitBy(formValidations, x => x === null),
+        validations: formValidations,
         isValid: this.formIsValid(fields, formValidations),
         isChanged: this.formIsChanged(fields)
       };
