9a997963bfe091d3bf72df1785b708ee38a01510
Francesco Giordano <francesco.giordano@buildo.io>
2017-04-03 14:47:09 +0200

    add onEnter handler to formo/dom/Input

--- src/formo/dom/Input.js
+++ src/formo/dom/Input.js
@@ -2,20 +2,30 @@ import React from 'react';
 import t, { maybe, union } from 'tcomb';
 import { props } from 'tcomb-react';
 import { skinnable, pure } from 'revenge';
+import noop from 'lodash/noop';
 
 @pure
 @skinnable()
 @props({
   value: maybe(union([t.Integer, t.String]), 'InputValue'),
-  onChange: t.Function
+  onChange: t.Function,
+  onEnter: maybe(t.Function)
 }, { strict: false })
 export default class Input extends React.Component {
 
-  template({ value, onChange, ...others }) {
+  template({ value, onChange, onEnter = noop, onKeyup = noop, ...others }) {
     return (
       <input
         value={t.Nil.is(value) ? '' : value}
         onChange={(e) => { onChange(e.target.value); }}
+        onKeyUp={(evt) => {
+          const { keyCode, which } = evt;
+          const isEnter = keyCode === 13 || which === 13;
+          if (isEnter) {
+            onEnter();
+          }
+          onKeyup(evt);
+        }}
         {...others}
       />
     );
