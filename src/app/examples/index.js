5ec15256d9faea995827ef13614102d2254e8434
Francesco Giordano <francesco.giordano@buildo.io>
2017-05-19 21:18:53 +0200

    remove unused examples

--- src/app/examples/index.js
+++ src/app/examples/index.js
@@ -1,13 +1,2 @@
 import MyForm from './MyForm';
-// import MyOtherForm from './MyOtherForm';
-// import AnotherOneOfMyForms from './AnotherOneOfMyForms';
-// import LastOneIPromise from './LastOneIPromise';
-// import INeverKeepPromisesForm from './INeverKeepPromisesForm';
-
-export {
-  // MyOtherForm,
-  // AnotherOneOfMyForms,
-  // LastOneIPromise,
-  // INeverKeepPromisesForm,
-  MyForm
-};
+export { MyForm };
